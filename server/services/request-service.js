07f86bb6b751056c64e947e5463bfde7ababc3db
luisb239 <luis.fpereira2@gmail.com>
2020-08-06 12:33:18 +0100

    Adds user to the notification

--- server/services/request-service.js
+++ server/services/request-service.js
@@ -147,10 +147,10 @@ module.exports = (requestDb, processDb, requestLanguagesDb, authModule, candidat
         await requestDb.addUserAndRoleToRequest({userId: userId, roleId: userRoles[0].RoleId, requestId: requestId})
     }
 
-    async function addUserToRequest({requestId, userId, roleId}) {
+    async function addUserToRequest({requestId, userId, roleId, currentUsername}) {
         await requestDb.addUserAndRoleToRequest({userId, roleId, requestId})
-        const request = requestDb.getRequestById({id: requestId})
-        await emailService.notifyAssigned({userId, request})
+        const request = await requestDb.getRequestById({id: requestId})
+        await emailService.notifyAssigned({userId, request, currentUsername})
     }
 
     async function updateRequestLanguages({requestId, languages, isMandatory}) {
